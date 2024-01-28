0a90f60f9acf47558b0d9fd63e1992f1b16d042f
Jacob Stephenson <jacobstephenson97@gmail.com>
2024-01-28 04:06:05 -0500

    Half way done

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,22 +20,35 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
     Append(usize),
 }
 
-mod my_module {
+pub mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
+        // let mut output: Vec<String> = vec![];
+        // for (string, command) in input.iter() {
+        //     match command {
+        //         Command::Append(num) => output.push(string.to_string() + &"bar".repeat(*num)),
+        //         Command::Trim => output.push(string.trim().to_string()),
+        //         Command::Uppercase => output.push(string.to_uppercase().to_string()),
+        //     }
+        //     // TODO: Complete the function body. You can do it!
+        // }
+        // output
+        let mut output: Vec<String> = vec![];
+        for (string, command) in input.into_iter() {
+            match command {
+                Command::Append(num) => output.push(string + &"bar".repeat(num)),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Uppercase => output.push(string.to_uppercase()),
+            }
             // TODO: Complete the function body. You can do it!
         }
         output
@@ -45,8 +58,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
